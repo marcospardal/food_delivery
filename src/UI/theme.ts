b29e9b3fc2133a5dbdf80d2f57f94d2ca9255e43
Marcos Davi <marcos.davi@kepha.com.br>
2022-04-18 12:20:16 -0300

    adding theme

---
+++ src/UI/theme.ts
@@ -0,0 +1,36 @@
+export const lightTheme = {
+  palette: {
+    common: {
+      white: '#FFFFFF',
+      black: '#000000'
+    },
+    primary: {
+      main: '#697BFF',
+      light: '#4E60FF'
+    },
+    contrastText: '#545563',
+    darkText: '#2B2B43'
+  },
+  typography: {
+    title: {
+      fontFamily: 'Nunito',
+      fontSize: '60px',
+      fontWeight: 'bold',
+      lineHeight: '82px',
+    },
+
+    description: {
+      fontFamily: 'Nunito',
+      fontSize: '14px',
+      fontWeight: 500,
+      lineHeight: '20px',
+    },
+
+    label: {
+      fontFamily: 'Nunito',
+      fontSize: '12px',
+      fontWeight: 600,
+      lineHeight: '16px',
+    }
+  }
+}
